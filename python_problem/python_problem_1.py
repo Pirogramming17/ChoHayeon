3c9fdae9264d9ea571c52465430c0356238aee30
likeyeon <chy98csy00@naver.com>
2022-07-08 16:17:29 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 num = 0
-cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+
+while True :   
+    try:
+        cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if cnt!=1 and cnt!=2 and cnt!=3 :
+            raise Exception
+        break
+    except ValueError :
+        print("정수를 입력하세요")
+    except Exception as e:
+        print("1,2,3 중 하나를 입력하세요", e)
