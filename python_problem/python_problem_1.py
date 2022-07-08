5ffea501807ccbad18b754ccb137f65cc3dd59d1
likeyeon <chy98csy00@naver.com>
2022-07-08 17:57:49 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,31 +1,41 @@
 # -*- coding: utf-8 -*-
 num = 0
 
-while True :   
-    try:
-        cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if cnt!=1 and cnt!=2 and cnt!=3 :
-            raise Exception
-        break
-    except ValueError :
-        print('정수를 입력하세요')
-    except Exception as e:
-        print('1,2,3 중 하나를 입력하세요', e)
+while True :
+    while True :   
+        try:
+            cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+            if cnt!=1 and cnt!=2 and cnt!=3 :
+                raise Exception
+            break
+        except ValueError :
+            print('정수를 입력하세요')
+        except Exception as e:
+            print('1,2,3 중 하나를 입력하세요', e)
 
-for i in range(num, cnt) :
-    print('playerA :', i+1)
-num += cnt
+    for i in range(num, num+cnt) :
+        num += 1
+        print('playerA :', i+1)
+        if num == 31:
+            break
+    if num == 31:
+        break 
 
-while True :   
-    try:
-        cnt2 = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if cnt2!=1 and cnt2!=2 and cnt2!=3 :
-            raise Exception
-        break
-    except ValueError :
-        print('정수를 입력하세요')
-    except Exception as e:
-        print('1,2,3 중 하나를 입력하세요', e)
+    while True :   
+        try:
+            cnt2 = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+            if cnt2!=1 and cnt2!=2 and cnt2!=3 :
+                raise Exception
+            break
+        except ValueError :
+            print('정수를 입력하세요')
+        except Exception as e:
+            print('1,2,3 중 하나를 입력하세요', e)
+    for i in range(num, num + cnt2) :
+        num += 1
+        print('playerB :', i+1)
+        if num == 31:
+            break
+    if num == 31:
+        break 
 
-for i in range(num, num + cnt2) :
-    print('playerB :', i+1)
