de23d1cfc20cc04b9fe04b9a43dcb35204a4e0b1
likeyeon <chy98csy00@naver.com>
2022-07-08 18:10:35 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,17 +2,20 @@
 num = 0
 
 while True :
-    while True :   
-        try:
-            cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-            if cnt!=1 and cnt!=2 and cnt!=3 :
-                raise Exception
-            break
-        except ValueError :
-            print('정수를 입력하세요')
-        except Exception as e:
-            print('1,2,3 중 하나를 입력하세요', e)
+    def brGame():
+        while True :   
+            try:
+                cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                return cnt
+                if cnt!=1 and cnt!=2 and cnt!=3 :
+                    raise Exception
+                break
+            except ValueError :
+                print('정수를 입력하세요')
+            except Exception as e:
+                print('1,2,3 중 하나를 입력하세요', e)
 
+    cnt = brGame()
     for i in range(num, num+cnt) :
         num += 1
         print('playerA :', i+1)
@@ -22,17 +25,8 @@ while True :
         print('playerB win!')
         break 
 
-    while True :   
-        try:
-            cnt2 = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-            if cnt2!=1 and cnt2!=2 and cnt2!=3 :
-                raise Exception
-            break
-        except ValueError :
-            print('정수를 입력하세요')
-        except Exception as e:
-            print('1,2,3 중 하나를 입력하세요', e)
-    for i in range(num, num + cnt2) :
+    cnt = brGame()
+    for i in range(num, num + cnt) :
         num += 1
         print('playerB :', i+1)
         if num == 31:
@@ -40,4 +34,6 @@ while True :
     if num == 31:
         print('playerA win!')
         break 
+
+
 
