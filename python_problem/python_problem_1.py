d08ab8b1b52eafab8f4f4722897b1415e7a886a7
likeyeon <chy98csy00@naver.com>
2022-07-08 16:43:15 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,14 +3,29 @@ num = 0
 
 while True :   
     try:
-        cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
         if cnt!=1 and cnt!=2 and cnt!=3 :
             raise Exception
         break
     except ValueError :
-        print("정수를 입력하세요")
+        print('정수를 입력하세요')
     except Exception as e:
-        print("1,2,3 중 하나를 입력하세요", e)
+        print('1,2,3 중 하나를 입력하세요', e)
 
 for i in range(num, cnt) :
     print('playerA :', i+1)
+num += cnt
+
+while True :   
+    try:
+        cnt2 = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if cnt2!=1 and cnt2!=2 and cnt2!=3 :
+            raise Exception
+        break
+    except ValueError :
+        print('정수를 입력하세요')
+    except Exception as e:
+        print('1,2,3 중 하나를 입력하세요', e)
+
+for i in range(num, num + cnt2) :
+    print('playerB :', i+1)
